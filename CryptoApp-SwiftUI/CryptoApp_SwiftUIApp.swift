e2455bfa0d7eeea714aff775c6852d26a1b51d8b
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-02-11 18:26:48 +0300

    Initial Commit

---
+++ CryptoApp-SwiftUI/CryptoApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp_SwiftUIApp.swift
+//  CryptoApp-SwiftUI
+//
+//  Created by Dmitry Yastrebov on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
